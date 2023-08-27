361243f1cf25f4c678bd92ccff5be7caaa89d85c
= <=>
2023-08-27 23:34:58 +0400

    add visivle reg button

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./redux-state/CartState";
 import { ChakraProvider } from "@chakra-ui/react";
 
-console.log(5);
+console.log(6);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

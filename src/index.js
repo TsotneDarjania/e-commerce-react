495020a084673b658f4fe4927ed9a3d6b2682c4a
= <=>
2023-08-27 23:34:15 +0400

    make footer section

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import store from "./redux-state/CartState";
 import { ChakraProvider } from "@chakra-ui/react";
 
-console.log(3);
+console.log(4);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
